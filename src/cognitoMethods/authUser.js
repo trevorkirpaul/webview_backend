208453b02260af90217f5ba0ba02e3b110cfeb8c
Trevor Kirpaul <trevorkirpaul@gmail.com>
2018-05-30 12:09:55 -0400

    pulled createUsed promise based helper into its own file

--- src/cognitoMethods/authUser.js
+++ src/cognitoMethods/authUser.js
@@ -4,37 +4,7 @@ const config = require('../config')
 global.fetch = require('node-fetch-polyfill')
 const { UserPoolId, ClientId } = config
 
-// setup
-// const poolData = {
-//   UserPoolId,
-//   ClientId
-// }
-// const userPool = new CognitoUserPool(poolData)
-// const Username = 'from1My@function.com'
-// const authData = {
-//   Username,
-//   Password: 'PASSword1987!'
-// }
-
-// const authDetails = new AmazonCognitoIdentity.AuthenticationDetails(authData)
-// const userData = {
-//   Username,
-//   Pool: userPool
-// }
-// const cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
-
-// method to auth user
-// cognitoUser.authenticateUser(authDetails, {
-//   onSuccess: function(result) {
-//     return console.log('TOKEN', result.getAccessToken().getJwtToken())
-//   },
-//   onFailure: function(err) {
-//     return console.log('ERROR', err)
-//   }
-// })
-
 // ? CREATE PROMISE BASED HELPER FXN
-
 const authCognitoUser = (Username, Password) => {
   const authDetails = new AmazonCognitoIdentity.AuthenticationDetails({
     Username,
@@ -56,6 +26,11 @@ const authCognitoUser = (Username, Password) => {
   })
 }
 
-authCognitoUser('fromMy@function.com', 'PASSword1987!')
-  .then(token => console.log('COMPLETE PROMISE', token))
-  .catch(err => console.log('FAIL PROMISE', err))
+// ? EXAMPLE USE OF AUTH USER FUNCTION
+// authCognitoUser('from1My@function.com', 'PASSword1987!')
+//   .then(token => console.log('COMPLETE PROMISE', token))
+//   .catch(err => console.log('FAIL PROMISE', err))
+
+module.exports = {
+  authCognitoUser
+}
---
+++ src/cognitoMethods/createUser.js
@@ -0,0 +1,38 @@
+const AmazonCognitoIdentity = require('amazon-cognito-identity-js')
+const CognitoUserPool = AmazonCognitoIdentity.CognitoUserPool
+const config = require('../config')
+global.fetch = require('node-fetch-polyfill')
+const { UserPoolId, ClientId } = config
+
+// init setup
+const poolData = {
+  UserPoolId, // found in 'General Settings' from cognito dashboard tab settings
+  ClientId  //found in 'App clients' from cognito dashboard tab settings
+}
+const userPool = new CognitoUserPool(poolData)
+
+const createCognitoUser = ({ username, password, email, phone }) => {
+  const attributeEmail = new AmazonCognitoIdentity.CognitoUserAttribute({ Name: 'email', Value: email })
+  const attributePhoneNumber = new AmazonCognitoIdentity.CognitoUserAttribute({ Name: 'phone_number', Value: phone })
+  const attributeList = [
+    attributeEmail,
+    attributePhoneNumber,
+  ]
+  let cognitoUser;
+  return promise = new Promise((resolve, reject) => {
+    userPool.signUp(username, password, attributeList, null, (err, result) => {
+      if (err) { return reject(err) }
+      cognitoUser = result.user
+      resolve(cognitoUser.getUsername())
+    })
+  })
+}
+
+// ? EXAMPLE USE OF CREATE / REGISTER FXN
+createCognitoUser({ username: 'newLocation@function.com', password: 'PASSword1987!', email: 'newLocation@function.com', phone: ''})
+  .then(data => console.log(data))
+  .catch(e => console.log(e))
+
+module.exports = {
+  createCognitoUser
+}
